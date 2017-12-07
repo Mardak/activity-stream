bd045bef10768fa475c7ff8ad176f63a68de5bcf
k88hudson <k88hudson@gmail.com>
2017-12-07 14:42:40 -0500

    convert system-addon/content-src/activity-stream.jsx

--- system-addon/content-src/activity-stream.jsx
+++ system-addon/content-src/activity-stream.jsx
@@ -1,13 +1,12 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const {Base} = require("content-src/components/Base/Base");
-const {Provider} = require("react-redux");
-const {reducers} = require("common/Reducers.jsm");
-const {actionTypes: at, actionCreators: ac} = require("common/Actions.jsm");
-
+import {actionCreators as ac, actionTypes as at} from "common/Actions.jsm";
+import {Base} from "content-src/components/Base/Base";
 import {DetectUserSessionStart} from "content-src/lib/detect-user-session-start";
+import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
 import {addSnippetsSubscriber} from "content-src/lib/snippets";
 import {initStore} from "content-src/lib/init-store";
+import {reducers} from "common/Reducers.jsm";
 
 const store = initStore(reducers, global.gActivityStreamPrerenderedState);
 
